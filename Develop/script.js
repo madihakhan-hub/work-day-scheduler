30704d86dc9f9e50962c952ca4b5d047e7b56f4c
madihakhan-hub <144630720+madihakhan-hub@users.noreply.github.com>
2024-04-24 20:54:21 -0500

    added javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,47 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
+  $(function () {
+    // Add a listener for click events on the save button
+    $(".saveBtn").click(function() {
+      var hour = $(this).parent().attr("id").split("-")[1];
+      var eventText = $(this).siblings(".description").val();
+      localStorage.setItem("event-" + hour, eventText);
+      alert("Event saved!");
+    });
+  
+    // Function to update time-block classes based on current hour
+    function updateHourlyBlocks() {
+      var currentHour = dayjs().hour();
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        if (blockHour < currentHour) {
+          $(this).removeClass("present future").addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).removeClass("past future").addClass("present");
+        } else {
+          $(this).removeClass("past present").addClass("future");
+        }
+      });
+    }
+  
+    // Load saved events from localStorage
+    for (var i = 9; i <= 17; i++) {
+      var savedEvent = localStorage.getItem("event-" + i);
+      if (savedEvent) {
+        $("#hour-" + i + " .description").val(savedEvent);
+      }
+    }
+  
+    // Display current date in the header of the page
+    var currentDay = dayjs().format("dddd, MMMM D");
+    $("#currentDay").text("Today is " + currentDay);
+  
+    // Call updateHourlyBlocks function when the page loads
+    updateHourlyBlocks();
+  
+    // Update time-block classes every minute
+    setInterval(updateHourlyBlocks, 60000);
+  });
+  
